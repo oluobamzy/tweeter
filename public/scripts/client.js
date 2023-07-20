c08851fb20bdbb52675df0f15e46395a71fd1640
oluobamzy <oluobamzy@gmail.com>
2023-07-19 22:50:05 -0400

    cross site scripting fixed, error messages handled

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,54 +4,58 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(function() {
-
   const renderTweets = (tweets) => {
     const $tweetsContainer = $('#loaded-articles');
 
     tweets.forEach((tweetData) => {
-      const $tweetElement = createTweetElement(tweetData);
-      $tweetsContainer.prepend($tweetElement);
+        const $tweetElement = createTweetElement(tweetData);
+        $tweetsContainer.prepend($tweetElement);
     });
   };
 
   const createTweetElement = (obj) => {
-    const tweet = $(` <article>
-    <header class="tweets-header">
-         <div class="profile-area">
-         <img src="${obj.user.avatars}" alt="Profile Image">
-         <p>${obj.user.name}</p>
-         </div>
-         <p>${obj.user.handle}</p>
-    </header>
-    <p class="display-text">${obj.content.text}</p>
-     <span class='line'></span>
-    <footer class="tweets-footer">
-    <p>${timeago.format(obj.created_at)}</p>
-       <div class="icons-footer">
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-       </div>
-
-    </footer>
-  </article>`);
-
-
+    const tweet = $(`<article>
+      <header class="tweets-header">
+        <div class="profile-area">
+          <img src="${escapeHTML(obj.user.avatars)}" alt="Profile Image">
+          <p>${escapeHTML(obj.user.name)}</p>
+        </div>
+        <p>${escapeHTML(obj.user.handle)}</p>
+      </header>
+      <p class="display-text">${escapeHTML(obj.content.text)}</p>
+      <span class='line'></span>
+      <footer class="tweets-footer">
+        <p>${escapeHTML(timeago.format(obj.created_at))}</p>
+        <div class="icons-footer">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`);
+  
     return tweet;
   };
+  
+  // Function to escape HTML entities
+  function escapeHTML(html) {
+    return $('<div>').text(html).html();
+  }
+  
 
   $("#myform").on('submit',function(event){
     event.preventDefault();
    // console.log(this);
+    
     const formData = $(this).serialize();
     let $counter = parseInt($('.counter').text());
     if ($counter < 0) {
-      alert("Only 140 characters are allowed❌❌❌");
+      $(".error-message").slideDown();
       return false; // Prevent form submission if count is negative.
     }
     
     if ($counter === 140) {
-      alert("Empty input");
+      $(".error-message").slideDown();
       return false; // Prevent form submission if count is exactly 140.
     }
 
@@ -62,10 +66,13 @@ $(function() {
       success: function(response) {
         // Handle the server response here
         console.log(response);
+        $(".error-message").slideUp();
+        loadTweets();
       },
       error: function(error) {
         // Handle errors here
         console.error(error);
+        $(".error-message").slideUp();
       }
     });
   });
@@ -80,6 +87,7 @@ $(function() {
     success: function(response){
       //handle response
       renderTweets(response);
+      $('#myform')[0].reset();
     },
     error: function(xhr,status,error){
         console.log(error);
@@ -90,7 +98,7 @@ $(function() {
   
   }
   loadTweets();
-
+  
 })
 
 
